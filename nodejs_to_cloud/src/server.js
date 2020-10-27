f7d3aac8778930ad824696da6c3f0cad314b7318
YPtashnik <gisohay626@dfb55.com>
2020-10-27 13:15:08 +0300

    formatted logs

--- nodejs_to_cloud/src/server.js
+++ nodejs_to_cloud/src/server.js
@@ -3,11 +3,11 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-  console.log('Hello World sent to Browser at: ' + new Date());
+  console.log('<<< Hello World sent to Browser at: ' + new Date());
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.info('<<<server is using Node.js version: ' + process.version);
-  console.info('<<<server listening on port ' + port);
+  console.info('<<< server is using Node.js version: ' + process.version);
+  console.info('<<< server listening on port ' + port);
 });
