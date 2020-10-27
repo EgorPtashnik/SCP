713815da2ccfa0d2a6af97adc815692f79e46b0f
YPtashnik <gisohay626@dfb55.com>
2020-10-27 13:12:20 +0300

    added some logic

--- nodejs_to_cloud/src/server.js
+++ nodejs_to_cloud/src/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
+  console.log('Hello World sent to Browser at: ' + new Date());
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.info('<<<server listening on port ' + port);
+  console.info('<<<server is using Node.js version: ' + process.version);
+  console.info('<<<server listening on port ' + port);
 });
