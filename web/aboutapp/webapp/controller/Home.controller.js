e5e814af8be49d943051049cbab3a501cd45ea80
YPtashnik <gisohay626@dfb55.com>
2020-11-05 13:16:28 +0300

    header

--- web/aboutapp/webapp/controller/Home.controller.js
+++ web/aboutapp/webapp/controller/Home.controller.js
@@ -1,7 +1,18 @@
 sap.ui.define([
-  "aboutapp/controller/BaseController"
-], function(BaseController) {
+  "aboutapp/controller/BaseController",
+  "sap/ui/model/json/JSONModel"
+], function(BaseController, JSONModel) {
   "use strict";
 
-  return BaseController.extend("aboutapp.controller.Home", {});
+  return BaseController.extend("aboutapp.controller.Home", {
+    onInit() {
+      this.setModel(new JSONModel({
+        email: "egor.ptashnik.webdev@gmail.com",
+        phone: "+375-29-978-46-60",
+        time: new Date(),
+        dateOfBirth: "17.10.1997"
+      }), "state");
+      setInterval(() => this.getModel("state").setProperty("/time", new Date()), 1000);
+    }
+  });
 });
