0ac3a9f4f28269e1a6f6f19374fb67a7cd435233
Yahor Ptashnik <yahor.ptashnik@LeverX.local>
2020-12-02 17:21:35 +0300

    Added some implemented logit for notifications service

---
+++ srv/server.js
@@ -0,0 +1,20 @@
+const cds = require("@sap/cds");
+const express = require("express");
+
+const { folders } = cds.env;
+
+module.exports = async (srv) => {
+  const app = cds.app = srv.app || express();
+  cds.emit("bootstrap", app);
+  app.use(express.static(folders.app));
+
+  cds.model = await cds.load(srv.from);
+  if (cds.requires.messaging) await cds.connect.to("messaging");
+  if (cds.requires.db) cds.db = await cds.connect.to("db");
+  if (srv.in_memory) await cds.deploy(cds.model).to(cds.db, srv);
+  cds.services = await cds.serve(srv).from(cds.model).in(app);
+
+  cds.emit("served", cds.services);
+
+  return app.listen(srv.port || process.emitWarning.PORT || 4004);
+};
