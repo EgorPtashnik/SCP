b3abaab2ee401f966031a1ee5a128808eb7cbd36
Yahor Ptashnik <yahor.ptashnik@LeverX.local>
2020-12-08 18:30:42 +0300

    Created UI for creating new notification

--- web/notificationapp/webapp/controller/Notification.controller.js
+++ web/notificationapp/webapp/controller/Notification.controller.js
@@ -1,10 +1,49 @@
 sap.ui.define([
-  "notificationapp/controller/BaseController"
-], function(BaseController) {
+  "notificationapp/controller/BaseController",
+  "notificationapp/utils/utils"
+], function(BaseController, Utils) {
   "use strict";
 
   return BaseController.extend("notificationapp.controller.Notification", {
-    onInit() {
+    toggleForm() {
+      if (!this.state.getProperty("/notification/data/Id")) this._generateUUID();
+      this.state.setProperty("/notification/new", !this.state.getProperty("/notification/new"));
+    },
+    handleSubmitNewNotification() {
+      const oNewNotificationData = this.state.getProperty("/notification/data");
+      console.log(oNewNotificationData);
+      this._restoreState();
+    },
+    _restoreState() {
+      this.state.setProperty("/notification/data", {
+        Id: "",
+        OriginId: "",
+        CreateAt: "",
+        IsActionable: false,
+        IsRead: false,
+        IsGroupable: false,
+        IsGroupHeader: false,
+        NavigationTargetAction: "",
+        NavigationTargetObject: "",
+        NavigationIntent: "",
+        NotificationTypeId: "",
+        NotificationTypeKey: "",
+        ParentId: "",
+        Priority: "None",
+        SensitiveText: "",
+        Text: "",
+        GroupHeaderText: "",
+        NotificationCount: 1,
+        SubTitle: "",
+        NotificationTypeDesc: "",
+        Actor_Id: "",
+        Actor_DisplayText: "",
+        Actor_ImageSource: ""
+      });
+      this.state.setProperty("/notification/new", false);
+    },
+    _generateUUID() {
+      this.state.setProperty("/notification/data/Id", Utils.generateUUID());
     }
   });
 });
--- web/notificationapp/webapp/model/models.js
+++ web/notificationapp/webapp/model/models.js
@@ -14,7 +14,41 @@ sap.ui.define([
       const oModel = new JSONModel({
         navMenuExpanded: false,
         navMenuItems: [],
-        selectedPage: "nHome"
+        selectedPage: "nHome",
+        notification: {
+          priorities: [
+            "None",
+            "Low",
+            "Medium",
+            "High"
+          ],
+          new: false,
+          data: {
+            Id: "",
+            OriginId: "",
+            CreateAt: "",
+            IsActionable: false,
+            IsRead: false,
+            IsGroupable: false,
+            IsGroupHeader: false,
+            NavigationTargetAction: "",
+            NavigationTargetObject: "",
+            NavigationIntent: "",
+            NotificationTypeId: "",
+            NotificationTypeKey: "",
+            ParentId: "",
+            Priority: "None",
+            SensitiveText: "",
+            Text: "",
+            GroupHeaderText: "",
+            NotificationCount: 1,
+            SubTitle: "",
+            NotificationTypeDesc: "",
+            Actor_Id: "",
+            Actor_DisplayText: "",
+            Actor_ImageSource: ""
+          }
+        }
       });
       return oModel;
     }
